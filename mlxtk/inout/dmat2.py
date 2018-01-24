3085b08be50a6377dbc0fabd352a6b695ccedcf4
Fabian Köhler <fkoehler1024@googlemail.com>
2018-01-24 20:38:58 +0100

    Plotting script and IO for dmat2

--- mlxtk/inout/dmat2.py
+++ mlxtk/inout/dmat2.py
@@ -1,20 +1,28 @@
 import pandas
 
+from . import hdf5
 
-def read_raw(path):
-    # read the complete file
+
+def read_dmat2(path):
+    parsed = hdf5.parse_hdf5_path(path)
+    if parsed is None:
+        return read_dmat2_ascii(path)
+    else:
+        read_dmat2_hdf5(parsed)
+
+
+def read_dmat2_ascii(path):
     data = pandas.read_csv(
         path,
         delimiter=r"\s+",
         header=None,
         names=["time", "dof1", "dof2", "element"])
+    # time = data["time"][0]
+    data.drop("time", 1, inplace=True)
+    return data
 
-    # get a list of all time points
-    times = data.time.unique().tolist()
 
-    # create a data frame for each time point
-    matrices = []
-    for time in times:
-        matrices.append(pandas.DataFrame(data.loc[data.time == time]))
-
-    return matrices
+def read_dmat2_hdf5(parsed_path):
+    path, path_inside = parsed_path
+    if not hdf5.is_hdf5_group(path, path_inside):
+        raise RuntimeError("Expected a group containing the output")

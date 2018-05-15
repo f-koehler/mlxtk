dd6198bb8dbec4fabeceeed0229e4008cd24c4da
Fabian Köhler <fkoehler@physnet.uni-hamburg.de>
2018-05-15 16:59:55 +0200

    Separate the code to compute gpop_diff from the plotting routine

--- mlxtk/plot/gpop.py
+++ mlxtk/plot/gpop.py
@@ -2,6 +2,7 @@ import numpy
 import scipy
 
 from ..inout.gpop import read_gpop
+from ..tools.gpop_diff import compute_absolute_gpop_diff, compute_relative_gpop_diff
 from .. import log
 
 
@@ -17,9 +18,10 @@ def plot_gpop(plot, path, dof):
 
 
 def plot_gpop_diff(plot, path1, path2, dof, relative=False):
+    """
+    .. todo:: allow vmin != -vmax but still keep the white color for a value of 0
+    """
     logger = log.get_logger(__name__)
-
-    threshold = 1e-5
 
     times1, grids1, densities1 = read_gpop(path1)
     times2, grids2, densities2 = read_gpop(path2)
@@ -27,86 +29,21 @@ def plot_gpop_diff(plot, path1, path2, dof, relative=False):
     grid1 = grids1[dof]
     grid2 = grids2[dof]
 
-    interpolate = False
-    if not numpy.array_equal(times1, times2):
-        if not numpy.array_equal(grids1[dof], grids2[dof]):
-            logger.warn("incompatible time and grid points, interpolating")
-        else:
-            logger.warn("incompatible time points, interpolating")
-        interpolate = True
-    elif not numpy.array_equal(grids1[dof], grids2[dof]):
-        logger.warn("incompatible grid points, interpolating")
-        interpolate = True
-
-    if not interpolate:
-        if relative:
-            mask = numpy.logical_or(densities1[dof] > threshold,
-                                    densities2[dof] > threshold)
-            values = numpy.zeros_like(densities1[dof])
-            values[mask] = 1. - densities2[dof][mask] / densities1[dof][mask]
-        else:
-            values = densities1[dof] - densities2[dof]
-        values = values.transpose()
-        amplitude = max(abs(numpy.min(values)), numpy.max(values))
-        t, x = numpy.meshgrid(times1, grids1[dof])
-        heatmap = plot.axes.pcolormesh(
-            t, x, values, cmap="bwr", vmin=-amplitude, vmax=amplitude)
-        plot.axes.set_xlabel("$t$")
-        plot.axes.set_ylabel("$x$")
-        cbar = plot.figure.colorbar(heatmap)
-        if relative:
-            cbar.ax.set_ylabel(
-                r"$1-\frac{\rho_1^{(2)}(x,t)}{\rho_1^{(1)}(x,t)}$")
-        else:
-            cbar.ax.set_ylabel(r"$\rho_1^{(1)}(x,t)-\rho_1^{(2)}(x,t)$")
+    if relative:
+        label = r"$1-\frac{\rho_1^{(2)}(x,t)}{\rho_1^{(1)}(x,t)}$"
+        t, x, values = compute_relative_gpop_diff(
+            times1, times2, grid1, grid2, densities1[dof], densities2[dof])
     else:
-        t_min = max(numpy.min(times1), numpy.min(times2))
-        t_max = min(numpy.max(times1), numpy.max(times2))
-        x_min = max(numpy.min(grid1), numpy.min(grid2))
-        x_max = min(numpy.max(grid1), numpy.max(grid2))
-        n_t = max(len(times1), len(times2))
-        n_x = max(len(grid1), len(grid2))
+        label = r"$\rho_1^{(1)}(x,t)-\rho_1^{(2)}(x,t)$"
+        t, x, values = compute_absolute_gpop_diff(
+            times1, times2, grid1, grid2, densities1[dof], densities2[dof])
 
-        gpop1 = numpy.transpose(densities1[dof])
-        gpop2 = numpy.transpose(densities2[dof])
+    amplitude = max(abs(values.min()), abs(values.max()))
 
-        interp1 = scipy.interpolate.interp2d(
-            times1,
-            grid1,
-            gpop1,
-            kind="quintic",
-            copy=False,
-            bounds_error=True)
-        interp2 = scipy.interpolate.interp2d(
-            times2,
-            grid2,
-            gpop2,
-            kind="quintic",
-            copy=False,
-            bounds_error=True)
-
-        t = numpy.linspace(t_min, t_max, n_t)
-        x = numpy.linspace(x_min, x_max, n_x)
-        if relative:
-            g1 = interp1(t, x)
-            g2 = interp2(t, x)
-            mask = numpy.logical_or(g1 > threshold, g2 > threshold)
-            values = numpy.zeros_like(g1)
-            values[mask] = (1. - g2 / g1)[mask]
-        else:
-            values = interp1(t, x) - interp2(t, x)
-        values = values.transpose()
-        amplitude = max(abs(numpy.min(values)), numpy.max(values))
-
-        heatmap = plot.axes.pcolormesh(
-            t, x, values, cmap="bwr", vmin=-amplitude, vmax=amplitude)
-        plot.axes.set_xlabel("$t$")
-        plot.axes.set_ylabel("$x$")
-        cbar = plot.figure.colorbar(heatmap)
-        if relative:
-            cbar.ax.set_ylabel(
-                r"$1-\frac{\rho_1^{(2)}(x,t)}{\rho_1^{(1)}(x,t)}$ (interpolated)"
-            )
-        else:
-            cbar.ax.set_ylabel(
-                r"$\rho_1^{(1)}(x,t)-\rho_1^{(2)}(x,t)$ (interpolated)")
+    plot.axes.set_xlabel("$t$")
+    plot.axes.set_ylabel("$x$")
+    T, X = numpy.meshgrid(t, x)
+    heatmap = plot.axes.pcolormesh(
+        T, X, values.transpose(), cmap="bwr", vmin=-amplitude, vmax=amplitude)
+    cbar = plot.figure.colorbar(heatmap)
+    cbar.ax.set_ylabel(label)
---
+++ mlxtk/tools/gpop_diff.py
@@ -0,0 +1,147 @@
+import numpy
+import scipy
+
+from .. import log
+
+LOGGER = log.get_logger(__name__)
+CLOSE_RTOL = 1e-5
+CLOSE_ATOL = 1e-8
+
+
+def compute_absolute_gpop_diff(time1, time2, grid1, grid2, gpop1, gpop2):
+    """Compute the absolute difference between one-body densities :math:`\\rho_1(x,t)-\\rho_2(x,t)`
+
+    When incompatible grids/times are specified, interpolation routines are applied.
+
+    Args:
+        time1: time points of the first density
+        time2: time points of the second density
+        grid1: grid points of the first density
+        grid2: grid points of the second density
+        gpop1: first density
+        gpop2: second density
+
+    Returns:
+        Time points, grid points, absolute difference between the densities
+    """
+    interpolate = False
+
+    if len(time1) != len(time2):
+        LOGGER.warn("different number of time points, interpolate")
+        interpolate = True
+    elif len(grid1) != len(grid2):
+        LOGGER.warn("different number of grid points, interpolate")
+        interpolate = True
+    elif not numpy.allclose(time1, time2):
+        LOGGER.warn(
+            "time points are not close to each other (rtol={}, atol={}), interpolate".
+            format(CLOSE_RTOL, CLOSE_ATOL))
+        interpolate = True
+    elif not numpy.allclose(grid1, grid2):
+        LOGGER.warn(
+            "grid points are not close to each other (rtol={}, atol={}), interpolate".
+            format(CLOSE_RTOL, CLOSE_ATOL))
+        interpolate = True
+
+    if interpolate:
+        den1 = numpy.transpose(gpop1)
+        den2 = numpy.transpose(gpop2)
+
+        interp1 = scipy.interpolate.interp2d(
+            time1, grid1, den1, kind="quintic", copy=False, bounds_error=True)
+        interp2 = scipy.interpolate.interp2d(
+            time2, grid2, den2, kind="quintic", copy=False, bounds_error=True)
+
+        t_min = max(time1.min(), time2.min())
+        t_max = min(time1.max(), time2.max())
+        x_min = max(grid1.min(), grid2.min())
+        x_max = min(grid1.max(), grid2.max())
+        n_t = max(len(time1), len(time2))
+        n_x = max(len(grid1), len(grid2))
+
+        t = numpy.linspace(t_min, t_max, n_t)
+        x = numpy.linspace(x_min, x_max, n_x)
+
+        return t, x, numpy.transpose(interp1(t, x) - interp2(t, x))
+    else:
+        return time1.copy(), grid1.copy(), gpop1 - gpop2
+
+
+def compute_relative_gpop_diff(time1,
+                               time2,
+                               grid1,
+                               grid2,
+                               gpop1,
+                               gpop2,
+                               threshold=1e-5):
+    """Compute the relative difference between one-body densities :math:`1-\\frac{\\rho_2(x,t)}{\\rho_1(x,t)}`
+
+    Only points where :math:`\\rho_1(x,t)` or :math:`\\rho_2(x,t)` excels the threshold are taken into account to avoid divergences.
+    When incompatible grids/times are specified, interpolation routines are applied.
+
+    Args:
+        time1: time points of the first density
+        time2: time points of the second density
+        grid1: grid points of the first density
+        grid2: grid points of the second density
+        gpop1: first density
+        gpop2: second density
+        threshold:
+
+    Returns:
+        Time points, grid points, relative difference between the densities
+    """
+    interpolate = False
+
+    if len(time1) != len(time2):
+        LOGGER.warn("different number of time points, interpolate")
+        interpolate = True
+    elif len(grid1) != len(grid2):
+        LOGGER.warn("different number of grid points, interpolate")
+        interpolate = True
+    elif not numpy.allclose(time1, time2):
+        LOGGER.warn(
+            "time points are not close to each other (rtol={}, atol={}), interpolate".
+            format(CLOSE_RTOL, CLOSE_ATOL))
+        interpolate = True
+    elif not numpy.allclose(grid1, grid2):
+        LOGGER.warn(
+            "grid points are not close to each other (rtol={}, atol={}), interpolate".
+            format(CLOSE_RTOL, CLOSE_ATOL))
+        interpolate = True
+
+    if interpolate:
+        t_min = max(time1.min(), time2.min())
+        t_max = min(time1.max(), time2.max())
+        x_min = max(grid1.min(), grid2.min())
+        x_max = min(grid1.max(), grid2.max())
+        n_t = max(len(time1), len(time2))
+        n_x = max(len(grid1), len(grid2))
+
+        t = numpy.linspace(t_min, t_max, n_t)
+        x = numpy.linspace(x_min, x_max, n_x)
+
+        den1 = scipy.interpolate.interp2d(
+            time1,
+            grid1,
+            numpy.transpose(gpop1),
+            kind="quintic",
+            copy=False,
+            bounds_error=True)(t, x)
+        den2 = scipy.interpolate.interp2d(
+            time2,
+            grid2,
+            numpy.transpose(gpop2),
+            kind="quintic",
+            copy=False,
+            bounds_error=True)(t, x)
+
+        mask = numpy.logical_or(den1, den2)
+        values = numpy.zeros_like(den1)
+        values[mask] = 1. - den2[mask] / den1[mask]
+        return t, x, values
+    else:
+        mask = numpy.logical_or(gpop1 > threshold, gpop2 > threshold)
+        values = numpy.zeros_like(gpop1)
+        values[mask] = 1. - gpop1[mask] / gpop2[mask]
+        return time1.copy(), grid1.copy(), values

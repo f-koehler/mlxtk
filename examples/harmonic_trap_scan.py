5ae05370f598dea56412495aa249b8ba607dd7e3
Fabian Köhler <fkoehler@physnet.uni-hamburg.de>
2018-11-05 16:09:34 +0100

    Fix harmonic_trap_scan example.

--- examples/harmonic_trap_scan.py
+++ examples/harmonic_trap_scan.py
@@ -4,31 +4,56 @@ import mlxtk.parameters
 from mlxtk.systems.single_species.harmonic_trap import HarmonicTrap
 
 if __name__ == "__main__":
-    x = mlxtk.dvr.add_harmdvr(400, 1.0, 1.0)
+    x = mlxtk.dvr.add_harmdvr(225, 0.0, 0.3)
 
     parameters = HarmonicTrap.create_parameters()
     parameters.g = 0.1
-    parameters_quenched = HarmonicTrap.create_parameters()
+    parameters_quenched = parameters.copy()
     parameters_quenched.g = 0.2
-    parameters.add_parameter("m", 1, "number of single particle functions")
 
     def create_simulation(p):
         system = HarmonicTrap(p, x)
         system_quenched = HarmonicTrap(parameters_quenched, x)
 
-        sim = mlxtk.Simulation("")
-        sim += system.get_1b_hamiltonian("hamiltonian_1b")
-        sim += system.get_mb_hamiltonian("hamiltonian")
-        sim += system_quenched.get_mb_hamiltonian("hamiltonian_quenched")
-        sim += mlxtk.tasks.create_mctdhb_wave_function("initial",
-                                                       "hamiltonian_1b", 2, 5)
+        sim = mlxtk.Simulation("harmonic_trap")
+        sim += mlxtk.tasks.create_operator("hamiltonian_1b",
+                                           system.get_hamiltonian_1b())
+        sim += mlxtk.tasks.create_many_body_operator("hamiltonian",
+                                                     system.get_hamiltonian())
+        sim += mlxtk.tasks.create_many_body_operator(
+            "hamiltonian_quenched", system_quenched.get_hamiltonian())
+        sim += mlxtk.tasks.create_many_body_operator(
+            "com", system.get_center_of_mass_operator())
+        sim += mlxtk.tasks.create_many_body_operator(
+            "com2", system.get_center_of_mass_operator_squared())
+        sim += mlxtk.tasks.create_mctdhb_wave_function(
+            "initial", "hamiltonian_1b", parameters.N, parameters.m)
+        sim += mlxtk.tasks.create_many_body_operator(
+            "com", system_quenched.get_center_of_mass_operator())
+        sim += mlxtk.tasks.create_many_body_operator(
+            "com_2", system_quenched.get_center_of_mass_operator_squared())
         sim += mlxtk.tasks.improved_relax(
-            "gs_relax", "initial", "hamiltonian", 1, tfinal=1000.0, dt=0.01)
+            "gs_relax", "initial", "hamiltonian", "1", tfinal=1000.0, dt=0.01)
+        sim += mlxtk.tasks.propagate(
+            "propagate",
+            "gs_relax/final",
+            "hamiltonian_quenched",
+            tfinal=5.0,
+            dt=0.05,
+            psi=True,
+            keep_psi=True, )
+        with mlxtk.tasks.ExtractedPsi(sim, "propagate/psi"):
+            sim += mlxtk.tasks.compute_expectation_value(
+                "propagate/psi", "com")
+            sim += mlxtk.tasks.compute_expectation_value(
+                "propagate/psi", "com_2")
+            sim += mlxtk.tasks.compute_variance("propagate/com",
+                                                "propagate/com_2")
 
         return sim
 
     scan = mlxtk.ParameterScan(
         "harmonic_trap_scan",
         create_simulation,
-        mlxtk.parameters.generate_all(parameters, {"m": [1, 2, 3, 4, 5, 6]}), )
+        mlxtk.parameters.generate_all(parameters, {"m": [1, 2, 5]}), )
     scan.main()

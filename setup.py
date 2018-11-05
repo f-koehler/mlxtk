e3078782ba3dcd4ccf04d49331ab78bab50d3037
Fabian Köhler <fkoehler@physnet.uni-hamburg.de>
2018-11-05 15:20:34 +0100

    Add environment for current mlxtk version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="mlxtk",
-    version="0.0.0",
+    version="0.3.0",
     author="Fabian Köhler",
     author_email="fkoehler@physnet.uni-hamburg.de",
     description="Toolkit to design, run and analyze ML-MCTDH(X) simulations",

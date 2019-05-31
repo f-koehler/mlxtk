4602395f4a8b22cd619927d79c6aa7f19cd55d31
Fabian Köhler <fkoehler1024@googlemail.com>
2019-05-31 14:16:31 +0200

    Combine settings file from parent directories.

--- mlxtk/settings.py
+++ mlxtk/settings.py
@@ -6,35 +6,36 @@ import yaml
 from .util import memoize
 
 
+def create_empty_settings() -> Dict[Any, Any]:
+    return {"env": {}, "paths": {}, "units": {}}
+
+
 @memoize
-def find_settings(start_directory: Path = Path.cwd()) -> Optional[Path]:
+def find_settings(start_directory: Path = Path.cwd()) -> Optional[List[Path]]:
     start_directory = start_directory.resolve()
-    while True:
+    setting_files = []
+    while len(start_directory.parts) > 1:
         path = start_directory / "mlxtkrc"
         if path.exists():
-            return path
+            setting_files.append(path)
+        start_directory = start_directory.parent
+    path = start_directory / "mlxtkrc"
+    if path.exists():
+        setting_files.append(path)
+    return setting_files
 
-        new_directory = start_directory.parent
-        if new_directory == start_directory:
-            return None
 
-        start_directory = new_directory
-
-
-@memoize
-def load_settings(start_directory: Path = Path.cwd()) -> Dict[Any, Any]:
-    settings_path = find_settings()
-    if settings_path is None:
-        return {}
-
+def load_settings_file(settings_path: Path) -> Dict[Any, Any]:
+    directory = settings_path.parent
     with open(settings_path, "r") as fp:
         settings = yaml.load(fp, Loader=yaml.CLoader)
 
     if "paths" in settings:
         for path in settings["paths"]:
             settings["paths"][path] = (
-                Path(start_directory) /
-                Path(settings["paths"][path])).resolve()
+                Path(directory) / Path(settings["paths"][path])).resolve()
+    else:
+        settings["paths"] = {}
 
     if "env" in settings:
         for entry in settings["env"]:
@@ -49,6 +50,40 @@ def load_settings(start_directory: Path = Path.cwd()) -> Dict[Any, Any]:
                         (settings_path.parent /
                          Path(settings["env"][entry][1:])).resolve()
                     ]
+    else:
+        settings["env"] = {}
+
+    settings["units"] = settings.get("units", {})
+
+    return settings
+
+
+def merge_settings(first: Dict[Any, Any],
+                   second: Dict[Any, Any]) -> Dict[Any, Any]:
+    new = {key: second[key] for key in second}
+
+    for entry in first["paths"]:
+        new["paths"][entry] = first["paths"][entry]
+
+    for entry in first["env"]:
+        if isinstance(first["env"][entry], list):
+            new["env"][entry] = first["env"][entry] + new["env"].get(entry, [])
+        else:
+            new["env"][entry] = first["env"][entry]
+
+    for entry in first["units"]:
+        new["units"][entry] = first["units"][entry]
+
+    return new
+
+
+@memoize
+def load_settings(start_directory: Path = Path.cwd()) -> Dict[Any, Any]:
+    files = find_settings(start_directory)
+    settings = create_empty_settings()
+
+    for file_ in files:
+        settings = merge_settings(settings, load_settings_file(file_))
 
     return settings
 

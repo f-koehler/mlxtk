57092d5adbdf22628f047ef0ba7759f586527785
Fabian Köhler <fkoehler@physnet.uni-hamburg.de>
2019-06-27 15:28:29 +0200

    Add logging functionality to `TemporaryDir` class.

--- mlxtk/temporary_dir.py
+++ mlxtk/temporary_dir.py
@@ -2,17 +2,20 @@ import shutil
 from pathlib import Path
 from typing import Union
 
+from .log import get_logger
 from .util import make_path
 
 
 class TemporaryDir:
-    def __init__(self, path):
+    def __init__(self, path: Union[str, Path]):
+        self.logger = get_logger(__name__ + ".TemporaryDir")
         self.complete = False
         self.path = make_path(path).resolve()
         self.already_present = self.path.exists()
 
     def __enter__(self):
         if not self.already_present:
+            self.logger.info("create tempory dir: %s", str(self.path))
             self.path.mkdir()
 
         return self
@@ -23,4 +26,5 @@ class TemporaryDir:
         del etrace
 
         if self.complete:
+            self.logger.info("remove temporary dir: %s", str(self.path))
             shutil.rmtree(self.path)

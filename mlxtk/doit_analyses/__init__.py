bbdc7bc7a12969a4a4ccdebd1058f00494a24f8a
Fabian Köhler <fkoehler@physnet.uni-hamburg.de>
2019-08-26 15:27:56 +0200

    Do not import methods directly into `mlxtk.doit_analyses`.

--- mlxtk/doit_analyses/__init__.py
+++ mlxtk/doit_analyses/__init__.py
@@ -1,6 +1,1 @@
-from . import fixed_ns
-from .expval import collect_final_expval, scan_plot_expval
-from .gpop import scan_plot_gpop
-from .max_depletion import collect_max_depletion, plot_max_depletion
-from .natpop import scan_plot_natpop
-from .plot import direct_plot
+from . import collect, expval, fixed_ns, gpop, max_depletion, natpop, plot

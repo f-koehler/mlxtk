5a59a6e75d9551f938571de78e5bd42e69ed1523
Fabian Köhler <fkoehler@physnet.uni-hamburg.de>
2019-09-03 14:32:54 +0200

    Update example dodo file.

--- examples/scan/dodo.py
+++ examples/scan/dodo.py
@@ -2,8 +2,13 @@ import mlxtk
 
 
 def task_plot_gpop():
-    return mlxtk.doit_analyses.scan_plot_gpop("harmonic_trap_scan")
+    return mlxtk.doit_analyses.gpop.scan_plot_gpop("harmonic_trap_scan")
 
 
 def task_plot_natpop():
-    return mlxtk.doit_analyses.scan_plot_natpop("harmonic_trap_scan")
+    return mlxtk.doit_analyses.natpop.scan_plot_natpop("harmonic_trap_scan")
+
+
+def task_analyse_natpop():
+    return mlxtk.doit_analyses.natpop.DefaultNatpopAnalysis(
+        "harmonic_trap_scan", "propagate")()

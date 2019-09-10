45bb31c822e159072a9077875d050e6b08267d39
Fabian Köhler <fkoehler@physnet.uni-hamburg.de>
2019-09-10 12:01:29 +0200

    Refactor `thin_out_psi` script.

--- mlxtk/scripts/thin_out_psi.py
+++ mlxtk/scripts/thin_out_psi.py
@@ -1,5 +1,8 @@
 import argparse
 import os.path
+import re
+
+import numpy
 
 from ..inout import read_psi_ascii, write_psi_ascii
 from ..log import get_logger
@@ -13,32 +16,67 @@ def main():
                         dest="input_",
                         type=str,
                         help="input psi file")
-    parser.add_argument("output", type=str, help="output psi file")
-    parser.add_argument("--imin",
-                        default=0,
-                        type=int,
-                        help="starting index for new psi")
-    parser.add_argument("--imax", type=int, help="stopping index for new psi")
-    parser.add_argument("--di",
-                        default=2,
-                        type=int,
-                        help="index stride for new psi")
+    parser.add_argument("-o", "--output", type=str, help="output psi file")
+    parser.add_argument("-t",
+                        "--time",
+                        type=float,
+                        action="append",
+                        help="time point for psi")
+    parser.add_argument("-s",
+                        "--slice",
+                        dest="slice_",
+                        type=str,
+                        action="append",
+                        help="index slices for new psi")
     args = parser.parse_args()
 
+    initial_size = os.path.getsize(os.path.realpath(args.input_))
     tape, times, psi = read_psi_ascii(args.input_)
-    initial_size = os.path.getsize(args.input_)
     LOGGER.info("starting with %d time steps", len(times))
 
-    if args.imax is None:
-        args.imax = len(times)
+    indices = set()
 
-    times = times[args.imin:args.imax:args.di]
-    psi = psi[args.imin:args.imax:args.di]
-    LOGGER.info("ending up with %d time steps", len(times))
+    if args.time:
+        for time in args.time:
+            LOGGER.info("add time %s", time)
+            indices.add(numpy.abs(times - time).argmin())
+
+    if args.slice_:
+        re_slice = re.compile(r"^([+-]*\d*):([+-]*\d*)(?::([+-]*\d*))?$")
+        for slice_ in args.slice_:
+            LOGGER.info("add slice: %s", slice_)
+            m = re_slice.match(slice_)
+            if not m:
+                raise RuntimeError("Invalid slice: \"%s\"", slice_)
+            start = 0
+            stop = len(times)
+            step = 1
+            if m.group(1) != "":
+                start = int(m.group(1))
+            if m.group(2) != "":
+                stop = int(m.group(2))
+            try:
+                if m.group(3) != "":
+                    step = int(m.group(3))
+            except IndexError:
+                pass
+
+            LOGGER.info("start: %d, stop: %d, step: %d", start, stop, step)
+
+            for i in range(start, stop, step):
+                indices.add(i)
+
+    if not indices:
+        raise RuntimeError("No frames for new psi file")
+
+    indices = list(indices)
+    indices.sort()
+    times = times[indices]
+    psi = psi[indices]
     write_psi_ascii(args.output, (tape, times, psi))
-    final_size = os.path.getsize(args.output)
-    LOGGER.info("file size reduced by {:.2f}%".format(
-        (1 - final_size / initial_size) * 100.))
+
+    final_size = os.path.getsize(os.path.realpath(args.output))
+    LOGGER.info("ending up with %d time steps", len(times))
 
 
 if __name__ == "__main__":

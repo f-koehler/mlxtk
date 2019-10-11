79747edb09b61185ad9ed470c7c3a034f7d55884
Fabian Köhler <fkoehler@physnet.uni-hamburg.de>
2019-10-11 17:57:18 +0200

    Bump version to 0.7.5.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="mlxtk",
-    version="0.7.4",
+    version="0.7.5",
     author="Fabian Köhler",
     author_email="fkoehler@physnet.uni-hamburg.de",
     description="Toolkit to design, run and analyze ML-MCTDH(X) simulations",

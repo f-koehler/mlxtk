11f5d9acb8b3587a7a9a069ca1cceb5eff9732e2
Fabian Köhler <fkoehler@physnet.uni-hamburg.de>
2019-11-15 18:20:37 +0100

    Add script to for dmat analysis and improve dmat I/O routines.

--- mlxtk/inout/dmat.py
+++ mlxtk/inout/dmat.py
@@ -5,8 +5,12 @@ import h5py
 import numpy
 import pandas
 
+from ..util import make_path
 
-def read_dmat_evals(path: str) -> Tuple[numpy.ndarray, numpy.ndarray]:
+
+def read_dmat_evals_ascii(path: Union[str, Path]
+                          ) -> Tuple[numpy.ndarray, numpy.ndarray]:
+    path = make_path(path)
     with open(path, "r") as fp:
         m = len(fp.readline().strip().split()) - 1
 
@@ -15,8 +19,18 @@ def read_dmat_evals(path: str) -> Tuple[numpy.ndarray, numpy.ndarray]:
     return df["time"].values, df[names[1:]].values
 
 
-def read_dmat_evecs_grid(
-        path: str) -> Tuple[numpy.ndarray, numpy.ndarray, numpy.ndarray]:
+def add_dmat_evals_to_hdf5(fptr: Union[h5py.File, h5py.Group],
+                           time: numpy.ndarray, eigenvalues: numpy.ndarray):
+    group = fptr.create_group("eigenvalues")
+    group.create_dataset("time", time.shape, time.dtype)[:] = time
+    group.create_dataset("eigenvalues", eigenvalues.shape,
+                         eigenvalues.dtype)[:, :] = eigenvalues
+
+
+def read_dmat_evecs_grid_ascii(
+        path: Union[str, Path]
+) -> Tuple[numpy.ndarray, numpy.ndarray, numpy.ndarray]:
+    path = make_path(path)
     with open(path, "r") as fp:
         m = (len(fp.readline().strip().split()) - 2) // 2
 
@@ -44,7 +58,59 @@ def read_dmat_evecs_grid(
     return times, grid, evecs.T.reshape(m, len(times), len(grid))
 
 
+def add_dmat_evecs_grid_to_hdf5(fptr: Union[h5py.File, h5py.Group],
+                                times: numpy.ndarray, grid: numpy.ndarray,
+                                evecs: numpy.ndarray):
+    group = fptr.create_group("evecs_grid")
+    group.create_dataset("time", times.shape, times.dtype)[:] = times
+    group.create_dataset("grid", grid.shape, grid.dtype)[:] = grid
+    group.create_dataset("real", evecs.shape,
+                         evecs.real.dtype)[:, :, :] = evecs.real
+    group.create_dataset("imag", evecs.shape,
+                         evecs.imag.dtype)[:, :, :] = evecs.imag
+
+
+def read_dmat_evecs_spf_ascii(path: Union[str, Path]
+                              ) -> Tuple[numpy.ndarray, numpy.ndarray]:
+    path = make_path(path)
+    with open(path, "r") as fp:
+        m = (len(fp.readline().strip().split()) - 2) // 2
+
+    names = ["time", "index"]
+    names_real = []
+    names_imag = []
+    for i in range(m):
+        names.append("real_" + str(i))
+        names.append("imag_" + str(i))
+        names_real.append("real_" + str(i))
+        names_imag.append("imag_" + str(i))
+    df = pandas.read_csv(path, delim_whitespace=True, header=None, names=names)
+    evecs = df[names_real].values + 1j * df[names_imag].values
+
+    num_indices = 0
+    t_0 = df["time"][0]
+    for t in df["time"]:
+        if t_0 != t:
+            break
+        num_indices += 1
+
+    times = df["time"].values[::num_indices]
+
+    return times, evecs.T.reshape(m, len(times), num_indices)
+
+
+def add_dmat_evecs_spf_to_hdf5(fptr: Union[h5py.File, h5py.Group],
+                               time: numpy.array, evecs: numpy.ndarray):
+    group = fptr.create_group("evecs_spf")
+    group.create_dataset("time", time.shape, time.dtype)[:] = time
+    group.create_dataset("real", evecs.shape,
+                         evecs.real.dtype)[:, :, :] = evecs.real
+    group.create_dataset("imag", evecs.shape,
+                         evecs.imag.dtype)[:, :, :] = evecs.imag
+
+
 def read_dmat_spfrep_ascii(path: Union[str, Path]):
+    path = make_path(path)
     df = pandas.read_csv(str(path),
                          delim_whitespace=True,
                          header=None,
@@ -57,6 +123,16 @@ def read_dmat_spfrep_ascii(path: Union[str, Path]):
     elements = df["real"].values + 1j * df["imag"].values
 
     return time, elements.reshape((num_times, num_i, num_j))
+
+
+def add_dmat_spfrep_to_hdf5(fptr: Union[h5py.File, h5py.Group],
+                            time: numpy.ndarray, dmat: numpy.ndarray):
+    group = fptr.create_group("dmat_spfrep")
+    group.create_dataset("time", time.shape, time.dtype)[:] = time
+    group.create_dataset("real", dmat.shape,
+                         dmat.real.dtype)[:, :, :] = dmat.real
+    group.create_dataset("imag", dmat.shape,
+                         dmat.imag.dtype)[:, :, :] = dmat.imag
 
 
 def read_dmat_gridrep_ascii(
@@ -96,14 +172,15 @@ def add_dmat_gridrep_to_hdf5(
         fptr: Union[h5py.File, h5py.Group],
         data: Tuple[numpy.ndarray, numpy.ndarray, numpy.ndarray, numpy.ndarray]
 ):
-    fptr.create_dataset("time", data[0].shape,
-                        dtype=numpy.float64)[:] = data[0]
-    fptr.create_dataset("x1", data[1].shape, dtype=numpy.float64)[:] = data[1]
-    fptr.create_dataset("x2", data[2].shape, dtype=numpy.float64)[:] = data[2]
-    fptr.create_dataset("real", data[3].shape,
-                        dtype=numpy.float64)[:, :, :] = data[3].real
-    fptr.create_dataset("imag", data[3].shape,
-                        dtype=numpy.float64)[:, :, :] = data[3].imag
+    group = fptr.create_group("dmat_gridrep")
+    group.create_dataset("time", data[0].shape,
+                         dtype=numpy.float64)[:] = data[0]
+    group.create_dataset("x1", data[1].shape, dtype=numpy.float64)[:] = data[1]
+    group.create_dataset("x2", data[2].shape, dtype=numpy.float64)[:] = data[2]
+    group.create_dataset("real", data[3].shape,
+                         dtype=numpy.float64)[:, :, :] = data[3].real
+    group.create_dataset("imag", data[3].shape,
+                         dtype=numpy.float64)[:, :, :] = data[3].imag
 
 
 def write_dmat_gridrep_hdf5(

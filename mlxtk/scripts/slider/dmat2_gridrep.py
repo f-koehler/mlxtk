0e55b74648c8ed6b84bc07eb15c61795ab0ba2fb
Fabian Köhler <fkoehler@physnet.uni-hamburg.de>
2019-11-18 19:00:41 +0100

    Update sliders for dmat/dmat2 (grid representation).

---
+++ mlxtk/scripts/slider/dmat2_gridrep.py
@@ -0,0 +1,28 @@
+import argparse
+import sys
+
+from PySide2 import QtWidgets
+
+from mlxtk import plot
+from mlxtk.inout.dmat2 import read_dmat2_gridrep_hdf5
+from mlxtk.scripts.slider import g1
+
+
+class Gui(g1.Gui):
+    pass
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path", nargs="?", default="dmat2.h5", help="path to the dmat file")
+    plot.add_argparse_2d_args(parser)
+    args = parser.parse_args()
+
+    app = QtWidgets.QApplication(sys.argv)
+    data = read_dmat2_gridrep_hdf5(args.path, "dmat2_gridrep")
+    gui = Gui(data, 0, len(data[0]) - 1)
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()

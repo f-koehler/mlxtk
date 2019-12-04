e65e7e948d50a1fa342c577078a52cd791b0eae9
Fabian Köhler <fkoehler@physnet.uni-hamburg.de>
2019-12-04 19:27:39 +0100

    Add another way to calculate two-body correlation functions.

--- mlxtk/scripts/compute/g2.py
+++ mlxtk/scripts/compute/g2.py
@@ -5,7 +5,7 @@ import h5py
 
 from ...inout import dmat, dmat2, g2
 from ...log import get_logger
-from ...tools.correlation import compute_g2
+from ...tools.correlation import compute_g2, compute_g2_diff
 
 LOGGER = get_logger(__name__)
 
@@ -15,13 +15,18 @@ def main():
     parser.add_argument("dmat", type=Path)
     parser.add_argument("dmat2", type=Path)
     parser.add_argument("-o", "--output", default="g2.h5", type=Path)
+    parser.add_argument("--diff", action="store_true")
     args = parser.parse_args()
 
     LOGGER.info("read dmat")
     data_dmat = dmat.read_dmat_gridrep_hdf5(args.dmat, "dmat_gridrep")
     LOGGER.info("read dmat2")
     data_dmat2 = dmat2.read_dmat2_gridrep_hdf5(args.dmat2, "dmat2_gridrep")
-    data = compute_g2(data_dmat, data_dmat2)
+
+    if args.diff:
+        data = compute_g2_diff(data_dmat, data_dmat2)
+    else:
+        data = compute_g2(data_dmat, data_dmat2)
 
     with h5py.File(args.output, "w") as fptr:
         g2.add_g2_to_hdf5(fptr, data)
--- mlxtk/tools/correlation.py
+++ mlxtk/tools/correlation.py
@@ -60,3 +60,32 @@ def compute_g2(
     LOGGER.info("finished computing g2")
 
     return time, x1, x2, g2
+
+
+def compute_g2_diff(
+        data_dmat: Tuple[numpy.ndarray, numpy.ndarray, numpy.ndarray, numpy.
+                         ndarray],
+        data_dmat2: Tuple[numpy.ndarray, numpy.ndarray, numpy.ndarray, numpy.
+                          ndarray]
+) -> Tuple[numpy.ndarray, numpy.ndarray, numpy.ndarray, numpy.ndarray]:
+    LOGGER.info("compute g2")
+
+    time, x1, x2, dmat = data_dmat
+    time_, x1_, x2_, dmat2 = data_dmat2
+
+    assert time.shape == time_.shape
+    assert x1.shape == x1_.shape
+    assert x2.shape == x2_.shape
+    assert numpy.allclose(time, time_)
+    assert numpy.allclose(x1, x1_)
+    assert numpy.allclose(x2, x2_)
+
+    g2 = numpy.zeros_like(dmat)
+    # TODO: improve this naive implementation (maybe numba can help)
+    for i in range(len(time)):
+        for j in range(len(x1)):
+            for k in range(len(x2)):
+                g2[i, j, k] = dmat2[i, j, k] - (dmat[i, j, j] * dmat[i, k, k])
+    LOGGER.info("finished computing g2")
+
+    return time, x1, x2, g2

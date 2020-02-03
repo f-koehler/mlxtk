88346c93e2accb73afb84fa39728c0312c353f78
Fabian Köhler <fkoehler@physnet.uni-hamburg.de>
2020-02-03 15:23:13 +0100

    Add function to compute dmat2 in SPF representation.

--- mlxtk/tools/tensors.py
+++ mlxtk/tools/tensors.py
@@ -48,10 +48,46 @@ def get_dmat_spf(
                 state_a = state.copy()
                 state_b = state.copy()
                 state_a[i] += 1
-                state_b[i] += 1
+                state_b[j] += 1
                 rho_1[i, j] = numpy.sqrt(
                     (state[i] + 1) * (state[j] + 1)) * numpy.conjugate(
                         coefficients[lookup.get_index(state_a)]
                     ) * coefficients[lookup.get_index(state_b)]
 
     return rho_1
+
+
+def get_dmat2_spf(
+        coefficients: numpy.ndarray, states_Nm2: numpy.ndarray,
+        lookup: ns_table.NumberStateLookupTableBosonic) -> numpy.ndarray:
+    m = len(states_Nm2[0])
+    rho_2 = numpy.zeros((m, m, m, m), dtype=numpy.complex128)
+    state_a = states_Nm2[0].copy()
+    state_b = states_Nm2[0].copy()
+
+    for i in range(m):
+        for j in range(m):
+            for k in range(m):
+                for l in range(m):
+                    for state in states_Nm2:
+                        state_a = state.copy()
+                        state_b = state.copy()
+                        delta_a = (1 if i == j else 0)
+                        delta_b = (1 if k == l else 0)
+                        factor_a = state[j] + 1 - delta_a
+                        if factor_a <= 0:
+                            continue
+                        factor_b = state[l] + 1 - delta_b
+                        if factor_b <= 0:
+                            continue
+                        state_a[i] += 1
+                        state_a[j] += 1
+                        state_b[k] += 1
+                        state_b[l] += 1
+                        rho_2[i, j, k, l] = numpy.sqrt(
+                            (state[i] + 1) * factor_a *
+                            (state[j] + 1) * factor_b) * numpy.conjugate(
+                                coefficients[lookup.get_index(state_a)]
+                            ) * coefficients[lookup.get_index(state_b)]
+
+    return rho_2

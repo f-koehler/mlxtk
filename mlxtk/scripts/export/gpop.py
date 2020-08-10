52dbba843c2604b325218216a95ed8dfe5da720f
Fabian Köhler <fabian.koehler@protonmail.ch>
2020-08-10 22:37:32 +0200

    Add export script for gpop.

---
+++ mlxtk/scripts/export/gpop.py
@@ -0,0 +1,27 @@
+import argparse
+from pathlib import Path
+from mlxtk.inout.gpop import read_gpop_hdf5
+import h5py
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--dof", type=int, default=1)
+    parser.add_argument("datafile", type=Path)
+    parser.add_argument("outputfile", type=Path)
+    args = parser.parse_args()
+
+    time, grid, densities = read_gpop_hdf5(args.datafile, "gpop", dof=args.dof)
+
+    args.outputfile.parent.mkdir(exist_ok=True, parents=True)
+
+    with h5py.File(args.outputfile, "w") as fptr:
+        fptr.create_dataset("time", shape=time.shape, dtype=time.dtype)[:] = time
+        fptr.create_dataset("grid", shape=grid.shape, dtype=grid.dtype)[:] = grid
+        fptr.create_dataset("density", shape=densities.shape, dtype=densities.dtype)[
+            :, :
+        ] = densities
+
+
+if __name__ == "__main__":
+    main()

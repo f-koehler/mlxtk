2196c7f7e36b75efc63cda8c3063a46668334638
Fabian Köhler <fabian.koehler@protonmail.ch>
2020-09-23 09:17:42 +0200

    Add pre-commit config.

--- doc/conf.py
+++ doc/conf.py
@@ -16,12 +16,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'mlxtk'
-copyright = '2019, Fabian Köhler <fkoehler@physnet.uni-hamburg.de>'
-author = 'Fabian Köhler <fkoehler@physnet.uni-hamburg.de>'
+project = "mlxtk"
+copyright = "2019, Fabian Köhler <fkoehler@physnet.uni-hamburg.de>"
+author = "Fabian Köhler <fkoehler@physnet.uni-hamburg.de>"
 
 # The full version, including alpha/beta/rc tags
-release = '0.5.0'
+release = "0.5.0"
 
 # -- General configuration ---------------------------------------------------
 
@@ -29,29 +29,32 @@ release = '0.5.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx.ext.autodoc", "sphinx.ext.coverage", "sphinx.ext.intersphinx",
-    "sphinx.ext.napoleon", "sphinx.ext.todo"
+    "sphinx.ext.autodoc",
+    "sphinx.ext.coverage",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.todo",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # intersphinx
 intersphinx_mapping = {

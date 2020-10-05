ba8dcc162a3c86ebe4d3882b59474f8d2cdbb43d
Fabian Köhler <fabian.koehler@protonmail.ch>
2020-10-05 16:06:36 +0200

    Add SQR example.

---
+++ examples/sqr/bose_hubbard.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import mlxtk
+from mlxtk import tasks
+from mlxtk.systems.sqr.bose_hubbard import BoseHubbardSQR
+
+parameters = BoseHubbardSQR.create_parameters()
+parameters.N = 4
+parameters.L = 4
+parameters.pbc = True
+parameters.add_parameter("penalty", 100, "coefficient of the penalty term")
+
+if __name__ == "__main__":
+    system = BoseHubbardSQR(parameters)
+
+    sim = mlxtk.Simulation("bose_hubbard")
+    sim += tasks.CreateOperator("hamiltonian.opr", system.create_hamiltonian())
+    sim += tasks.CreateSQRBosonicWaveFunction(
+        "initial.wfn", [5, 7, 8], parameters.N, parameters.L, [1, 1, 1, 1]
+    )
+    sim += tasks.Relax(
+        "gs_relax", "initial.wfn", "hamiltonian.opr", tfinal=1200, dt=0.1, statsteps=50
+    )
+
+    sim.main()

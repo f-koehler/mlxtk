0c53bd5ff4e1b5078ae6d88be9ce55b5d1764aae
Fabian Köhler <fabian.koehler@protonmail.ch>
2020-11-02 12:33:34 +0100

    Remove add momentum (split) code; this is not a well-defined transformation.

--- mlxtk/tools/wave_function.py
+++ mlxtk/tools/wave_function.py
@@ -30,26 +30,6 @@ def add_momentum(wfn: Wavefunction, momentum: float) -> Wavefunction:
     return wfn
 
 
-def add_momentum_split(
-    wfn: Wavefunction, momentum: float, x_center: float
-) -> Wavefunction:
-    # pylint: disable=protected-access
-    num_spfs = wfn.tree._subnodes[0]._dim  # type: int
-    len_spfs = wfn.tree._subnodes[0]._phiLen  # type: int
-    grid = wfn.tree._topNode._pgrid[0]
-
-    phase = numpy.exp(
-        1j * (2.0 * numpy.heaviside(grid - x_center, 0.5) - 1.0) * momentum * grid
-    )
-
-    for i in range(0, num_spfs):
-        start = wfn.tree._subnodes[0]._z0 + i * len_spfs
-        stop = start + len_spfs
-        wfn.PSI[start:stop] = phase * wfn.PSI[start:stop]
-
-    return wfn
-
-
 def get_spfs(wfn: Wavefunction) -> List[numpy.ndarray]:
     # pylint: disable=protected-access
     num_spfs = wfn.tree._subnodes[0]._dim  # type: int

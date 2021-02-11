dbb6f6d9f8e1a21d06163b50d6846f2188b95d23
Fabian Köhler <fabian.koehler@protonmail.ch>
2021-02-11 12:21:59 +0100

    Fix bug when numpy-stl is not installed.

--- mlxtk/plot/gpop.py
+++ mlxtk/plot/gpop.py
@@ -4,7 +4,13 @@ import matplotlib.colors
 import matplotlib.pyplot
 import matplotlib.tri
 import numpy
-import stl
+
+try:
+    import stl
+
+    HAS_STL = True
+except ModuleNotFoundError:
+    HAS_STL = False
 
 from mlxtk.tools.gpop import transform_to_momentum_space
 
@@ -51,26 +57,34 @@ def plot_gpop_momentum(ax, time, grid, density):
     ax.set_xlabel("$p$")
 
 
-def create_gpop_model(time, grid, density):
-    Y, X = numpy.meshgrid(grid / grid.max(), time / time.max())
-    Z = density / density.max()
+if HAS_STL:
 
-    fig, ax = matplotlib.pyplot.subplots(1, 1)
-    ax.pcolormesh(X, Y, Z, cmap="gnuplot")
-    ax.set_axis_off()
-    fig.tight_layout()
-    fig.savefig("test.png", dpi=1200)
-    matplotlib.pyplot.close()
+    def create_gpop_model(time, grid, density):
+        Y, X = numpy.meshgrid(grid / grid.max(), time / time.max())
+        Z = density / density.max()
 
-    X = X.flatten()
-    Y = Y.flatten()
-    tri = matplotlib.tri.Triangulation(X, Y)
-    Z = density.flatten()
-    Z = Z / Z.max() * 0.1
+        fig, ax = matplotlib.pyplot.subplots(1, 1)
+        ax.pcolormesh(X, Y, Z, cmap="gnuplot")
+        ax.set_axis_off()
+        fig.tight_layout()
+        fig.savefig("test.png", dpi=1200)
+        matplotlib.pyplot.close()
 
-    # , remove_empty_areas=False)
-    mesh = stl.mesh.Mesh(numpy.zeros(len(tri.triangles), dtype=stl.mesh.Mesh.dtype))
-    mesh.x[:] = X[tri.triangles]
-    mesh.y[:] = Y[tri.triangles]
-    mesh.z[:] = Z[tri.triangles]
-    mesh.save("test.stl")
+        X = X.flatten()
+        Y = Y.flatten()
+        tri = matplotlib.tri.Triangulation(X, Y)
+        Z = density.flatten()
+        Z = Z / Z.max() * 0.1
+
+        # , remove_empty_areas=False)
+        mesh = stl.mesh.Mesh(numpy.zeros(len(tri.triangles), dtype=stl.mesh.Mesh.dtype))
+        mesh.x[:] = X[tri.triangles]
+        mesh.y[:] = Y[tri.triangles]
+        mesh.z[:] = Z[tri.triangles]
+        mesh.save("test.stl")
+
+
+else:
+
+    def create_gpop_model(time, grid, density):
+        raise RuntimeError("stl module not installed")

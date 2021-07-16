9c054a1a379e3328333465942e8c6a5a86740b35
Fabian Köhler <fabian.koehler@protonmail.ch>
2021-07-16 15:27:32 +0200

    Add dynamic number state analysis.

--- mlxtk/tasks/__init__.py
+++ mlxtk/tasks/__init__.py
@@ -1,7 +1,10 @@
 from mlxtk.tasks.expval import ComputeExpectationValue, ComputeExpectationValueStatic
 from mlxtk.tasks.mb_operator import CreateMBOperator, MBOperatorSpecification
 from mlxtk.tasks.momentum_distribution import MCTDHBMomentumDistribution
-from mlxtk.tasks.number_state_analysis import NumberStateAnalysisStatic
+from mlxtk.tasks.number_state_analysis import (
+    NumberStateAnalysisStatic,
+    NumberStateAnalysis,
+)
 from mlxtk.tasks.operator import CreateOperator, OperatorSpecification
 from mlxtk.tasks.propagate import Diagonalize, ImprovedRelax, Propagate, Relax
 from mlxtk.tasks.spectrum import ComputeSpectrum
--- mlxtk/tasks/number_state_analysis.py
+++ mlxtk/tasks/number_state_analysis.py
@@ -88,3 +88,85 @@ class NumberStateAnalysisStatic(Task):
 
     def get_tasks_run(self) -> List[Callable[[], Dict[str, Any]]]:
         return [self.task_compute]
+
+
+class NumberStateAnalysis(Task):
+    def __init__(
+        self,
+        wave_function: Union[str, Path],
+        psi: Union[str, Path],
+        basis: Union[str, Path],
+        **kwargs,
+    ):
+        self.logger = get_logger(__name__ + ".NumberStateAnalysis")
+
+        self.wave_function = make_path(wave_function)
+        self.basis = make_path(basis)
+        self.psi = make_path(psi)
+        self.result = make_path(
+            kwargs.get(
+                "name",
+                self.psi.with_name(self.psi.stem + "_" + self.basis.stem),
+            )
+        ).with_suffix(".fixed_ns.h5")
+
+        self.name = str(self.result.with_suffix(""))
+
+    def task_compute(self) -> Dict[str, Any]:
+        # pylint: disable=protected-access
+
+        @DoitAction
+        def action_compute(targets: List[str]):
+            del targets
+
+            wave_function = self.wave_function.resolve()
+            psi = self.psi.resolve()
+            basis = self.basis.resolve()
+            result = self.result.resolve()
+
+            with tempfile.TemporaryDirectory() as tmpdir:
+                with cwd.WorkingDir(tmpdir):
+                    self.logger.info("perform number state analysis")
+                    copy_file(wave_function, "restart")
+                    copy_file(basis, "basis")
+                    copy_file(psi, "psi")
+                    cmd = [
+                        "qdtk_analysis.x",
+                        "-fixed_ns",
+                        "-rst_bra",
+                        "basis",
+                        "-rst_ket",
+                        "restart",
+                        "-save",
+                        "result",
+                        "-psi",
+                        "psi",
+                    ]
+                    self.logger.info("command: %s", " ".join(cmd))
+                    env = os.environ.copy()
+                    env["OMP_NUM_THREADS"] = env.get("OMP_NUM_THREADS", "1")
+                    subprocess.run(cmd, env=env)
+
+                    times, real, imag = inout.read_fixed_ns_ascii("result")
+                    wfn = load_wave_function("basis")
+                    inout.write_fixed_ns_hdf5(
+                        "result.h5", times, real, imag, wfn._tape[1], wfn._tape[3]
+                    )
+
+                    with h5py.File("result.h5", "a") as fptr:
+                        dset = fptr["fixed_ns"].create_dataset(
+                            "total_magnitude", shape=times.shape, dtype=numpy.float64
+                        )
+                        dset[:] = numpy.sum((real ** 2) + (imag ** 2), axis=1)
+
+                    copy_file("result.h5", result)
+
+        return {
+            "name": f"number_state_analysis:{self.name}:compute",
+            "actions": [action_compute],
+            "targets": [str(self.result)],
+            "file_dep": [str(self.wave_function), str(self.basis), str(self.psi)],
+        }
+
+    def get_tasks_run(self) -> List[Callable[[], Dict[str, Any]]]:
+        return [self.task_compute]

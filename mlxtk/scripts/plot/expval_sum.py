da1da1b8625958b352919a23f1fef9f4e357af5d
Fabian Köhler <fabian.koehler@protonmail.ch>
2022-02-01 17:40:25 +0100

    Add script to plot sum of expectation values.

---
+++ mlxtk/scripts/plot/expval_sum.py
@@ -0,0 +1,67 @@
+import argparse
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy
+import scipy.fftpack
+
+import mlxtk
+from mlxtk import units
+from mlxtk.inout.expval import read_expval_hdf5
+from mlxtk.plot import (
+    add_argparse_2d_args,
+    add_argparse_save_arg,
+    apply_2d_args,
+    handle_saving,
+    plot_expval,
+)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path", type=Path, nargs="+", help="path to the output file")
+    parser.add_argument(
+        "--fft",
+        action="store_true",
+        help="whether to transform the signal to frequency space",
+    )
+    parser.add_argument("--xname", help="", default="time")
+    add_argparse_2d_args(parser)
+    add_argparse_save_arg(parser)
+    args = parser.parse_args()
+
+    figure, ax = plt.subplots(1, 1)
+
+    unitsys = mlxtk.units.get_default_unit_system()
+
+    values_sum = None
+    for file in args.path:
+        name = file.stem
+        time, values = read_expval_hdf5(file, xname=args.xname)
+        if values_sum is None:
+            values_sum = values
+        else:
+            values_sum += values
+
+    if args.fft:
+        plot_expval(ax, *mlxtk.tools.signal.fourier_transform(time, values))
+
+        ax.set_xlabel((1 / unitsys.get_time_unit()).format_label(r"\omega"))
+        ax.set_ylabel(mlxtk.units.ArbitraryUnit().format_label(r"\mathrm{amplitude}"))
+    else:
+        plot_expval(ax, time, values)
+        if args.xname == "time":
+            ax.set_xlabel(unitsys.get_time_unit().format_label("t"))
+        else:
+            ax.set_xlabel(args.xname)
+
+    apply_2d_args(ax, figure, args)
+
+    handle_saving(figure, args)
+
+    if not args.output:
+        plt.show()
+
+
+if __name__ == "__main__":
+    main()

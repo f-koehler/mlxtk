ab7a72594de5b3825ad535025150c422b1bb0b8b
Fabian Köhler <fabian.koehler@protonmail.ch>
2022-05-03 17:48:22 +0200

    Add some observables to spin glass system.

--- mlxtk/systems/spin_half/spin_glass.py
+++ mlxtk/systems/spin_half/spin_glass.py
@@ -58,4 +58,60 @@ class DisorderedXYSpinGlass:
             table,
         )
 
+    def create_Sx_operator(self) -> OperatorSpecification:
+        return OperatorSpecification(
+            [self.grid] * self.parameters.L,
+            {f"Sx_coeff": 1.0},
+            {f"Sx_term": self.grid.get().get_sigma_x()},
+            [f"Sx_coeff | {site + 1} Sx_term" for site in range(self.parameters["L"])],
+        )
+
+    def create_Sz_operator(self) -> OperatorSpecification:
+        return OperatorSpecification(
+            [self.grid] * self.parameters.L,
+            {f"Sz_coeff": 1.0},
+            {f"Sz_term": self.grid.get().get_sigma_z()},
+            [f"Sz_coeff | {site + 1} Sz_term" for site in range(self.parameters["L"])],
+        )
+
+    def create_sx_operator(self, site: int) -> OperatorSpecification:
+        return OperatorSpecification(
+            [self.grid] * self.parameters.L,
+            {f"sx_coeff_{site}": 1.0},
+            {f"sx_term_{site}": self.grid.get().get_sigma_x()},
+            f"sx_coeff_{site} | {site + 1} sx_term_{site}",
+        )
+
+    def create_sy_operator(self, site: int) -> OperatorSpecification:
+        return OperatorSpecification(
+            [self.grid] * self.parameters.L,
+            {f"sy_coeff_{site}": 1.0},
+            {f"sy_term_{site}": self.grid.get().get_sigma_y()},
+            f"sy_coeff_{site} | {site + 1} sy_term_{site}",
+        )
+
+    def create_sz_operator(self, site: int) -> OperatorSpecification:
+        return OperatorSpecification(
+            [self.grid] * self.parameters.L,
+            {f"sz_coeff_{site}": 1.0},
+            {f"sz_term_{site}": self.grid.get().get_sigma_z()},
+            f"sz_coeff_{site} | {site + 1} sz_term_{site}",
+        )
+
+    def create_sx_sx_operator(self, site1: int, site2: int) -> OperatorSpecification:
+        return OperatorSpecification(
+            [self.grid] * self.parameters.L,
+            {f"sx_sx_coeff_{site1}_{site2}": 1.0},
+            {f"sx_sx_term_{site1}_{site2}": self.grid.get().get_sigma_x()},
+            f"sx_sx_coeff_{site1}_{site2} | {site1 + 1} sx_sx_term_{site1}_{site2}| {site2 + 1} sx_sx_term_{site1}_{site2}",
+        )
+
+    def create_sz_sz_operator(self, site1: int, site2: int) -> OperatorSpecification:
+        return OperatorSpecification(
+            [self.grid] * self.parameters.L,
+            {f"sz_sz_coeff_{site1}_{site2}": 1.0},
+            {f"sz_sz_term_{site1}_{site2}": self.grid.get().get_sigma_z()},
+            f"sz_sz_coeff_{site1}_{site2} | {site1 + 1} sz_sz_term_{site1}_{site2}| {site2 + 1} sz_sz_term_{site1}_{site2}",
+        )
+
 

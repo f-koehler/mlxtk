75e0c12c2f741564808a5b8f6e29684c5912a82b
Fabian Köhler <fabian.koehler@protonmail.ch>
2022-05-24 16:18:08 +0200

    Add AlternatingBinaryTree.

--- mlxtk/tape/square_lattice.py
+++ mlxtk/tape/square_lattice.py
@@ -71,3 +71,88 @@ def create_quad_tree(
         dof_map[(node.attrs["x"], node.attrs["y"])] = node.attrs["dof"]
 
     return top_node, dof_map
+
+
+@dataclass
+class AlternatingBinaryTreeLayer:
+    even: bool
+    size_x: int
+    size_y: int
+    nodes: dict[tuple[int, int], Node]
+
+    @staticmethod
+    def create_bottom(L: int, primitive_dim: int) -> AlternatingBinaryTreeLayer:
+        if (not L) or (L & (L - 1)):
+            raise ValueError("L must be a positive power of two")
+
+        return AlternatingBinaryTreeLayer(
+            True,
+            L,
+            L,
+            {
+                (x, y): PrimitiveNode(primitive_dim, attrs={"x": x, "y": y})
+                for x, y in itertools.product(range(L), range(L))
+            },
+        )
+
+    def create_parent(self, orbitals: int = 1):
+        if (self.size_x == 1) and (self.size_y == 1):
+            raise ValueError("Already reached top layer, cannot create parent layer")
+
+        if self.even:
+            new_size_x = self.size_x // 2
+            new_size_y = self.size_y
+            layer = AlternatingBinaryTreeLayer(
+                False,
+                new_size_x,
+                new_size_y,
+                {
+                    (x, y): NormalNode(orbitals, attrs={"x": x, "y": y})
+                    for x, y in itertools.product(range(new_size_x), range(new_size_y))
+                },
+            )
+            for x, y in itertools.product(range(new_size_x), range(new_size_y)):
+                layer.nodes[(x, y)] += self.nodes[(2 * x, y)]
+                layer.nodes[(x, y)] += self.nodes[(2 * x + 1, y)]
+        else:
+            new_size_x = self.size_x
+            new_size_y = self.size_y // 2
+            layer = AlternatingBinaryTreeLayer(
+                True,
+                new_size_x,
+                new_size_y,
+                {
+                    (x, y): NormalNode(orbitals, attrs={"x": x, "y": y})
+                    for x, y in itertools.product(range(new_size_x), range(new_size_y))
+                },
+            )
+            for x, y in itertools.product(range(new_size_x), range(new_size_y)):
+                layer.nodes[(x, y)] += self.nodes[(x, 2 * y)]
+                layer.nodes[(x, y)] += self.nodes[(x, 2 * y + 1)]
+
+        return layer
+
+
+def create_alternating_binary_tree(
+    L: int,
+    primitive_dim: int,
+    orbitals: list[int],
+) -> tuple[NormalNode, dict[tuple[int, int], int]]:
+    orbitals_ = orbitals.copy()
+    orbitals_.append(1)
+    top_layer = AlternatingBinaryTreeLayer.create_bottom(L, primitive_dim)
+    while (top_layer.size_x != 1) and (top_layer.size_y):
+        top_layer = top_layer.create_parent(orbitals.pop(0))
+
+    if orbitals_:
+        raise ValueError(f"Too many orbital numbers provided, remainder: {orbitals_}")
+
+    top_node = top_layer.nodes[(0, 0)]
+    top_node.compute_layers()
+    top_node.compute_dof_numbers()
+
+    dof_map: dict[tuple[int, int], int] = {}
+    for node in top_node.get_primitive_nodes():
+        dof_map[(node.attrs["x"], node.attrs["y"])] = node.attrs["dof"]
+
+    return top_node, dof_map
